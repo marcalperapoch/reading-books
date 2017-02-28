5fd87390e3f96d6c5ab4c5b608975374b2c34950
Marçal Perapoch Amadó <marcal.perapoch@Marcals-MacBook-Pro.local>
2017-02-28 11:37:49 +0100

    Added README.md and changed application port

---
+++ README.md
@@ -0,0 +1,9 @@
+# Reading list
+The aim of this project is to develop a small web application to understand how **Spring Boot** works and how to use it.
+
+## Run the app
+
+```gradle
+$ gradle build
+$ java -jar build/libs/readingList-0.0.1-SNAPSHOT.jar
+```
--- src/main/java/com/perapoch/ReadingListApplication.java
+++ src/main/java/com/perapoch/ReadingListApplication.java
@@ -3,7 +3,7 @@ package com.perapoch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // Enable component-scanning and auto-configuration
 public class ReadingListApplication {
 
 	public static void main(String[] args) {
